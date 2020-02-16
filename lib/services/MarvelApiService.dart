df7ce8349df7d4eaa34b53e867333dec50f75304
linnefromice <linnefromice@gmail.com>
2020-02-16 23:47:52 +0900

    add Service class for calling api (no logic)

---
+++ lib/services/MarvelApiService.dart
@@ -0,0 +1,37 @@
+class MarvelApiService {
+  final base_url = "https://gateway.marvel.com/v1/public/characters";
+  final characters = "characters";
+  final comics = "comics";
+  final creators = "creators";
+  final events = "events";
+  final series = "series";
+  final stories = "stories";
+
+  final ts = "1";
+  final public_key = "2897bec72a68cf322e6f4cba8b778ada";
+  final private_key = "XXXX";
+
+  static Future fetchCharacters() async {
+    // TODO: add logic
+  }
+
+  static Future fetchComics() async {
+    // TODO: add logic
+  }
+
+  static Future fetchCreators() async {
+    // TODO: add logic
+  }
+
+  static Future fetchEvents() async {
+    // TODO: add logic
+  }
+
+  static Future fetchSeries() async {
+    // TODO: add logic
+  }
+
+  static Future fetchStories() async {
+    // TODO: add logic
+  }
+}
