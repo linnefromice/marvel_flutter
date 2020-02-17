add7c3b792ec222f5eb3d00aefe86d6f25722aa3
linnefromice <linnefromice@gmail.com>
2020-02-17 17:38:04 +0900

    modify ResourceList & Items model

--- lib/models/Items.dart
+++ lib/models/Items.dart
@@ -1,7 +1,21 @@
 class Items {
-  final String name;
-  final String type;
-  final String resourceURI;
+  String name;
+  String type;
+  String resourceURI;
 
-  Items(this.name, this.type, this.resourceURI);
+  Items({this.name, this.type, this.resourceURI});
+
+  Items.fromJson(Map<String, dynamic> json) {
+    name = json['name'];
+    type = json['type'];
+    resourceURI = json['resourceURI'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['resourceURI'] = this.resourceURI;
+    data['name'] = this.name;
+    data['type'] = this.type;
+    return data;
+  }
 }
--- lib/models/ResourceList.dart
+++ lib/models/ResourceList.dart
@@ -1,6 +1,36 @@
+import 'package:marvel_flutter/models/Items.dart';
+
 // https://developer.marvel.com/documentation/entity_types
 // -> Resource Lists
-
 class ResourceList {
+  int available;
+  String collectionURI;
+  List<Items> items;
+  int returned;
 
+  ResourceList(
+      {this.available, this.collectionURI, this.items, this.returned});
+
+  ResourceList.fromJson(Map<String, dynamic> json) {
+    available = json['available'];
+    collectionURI = json['collectionURI'];
+    if (json['items'] != null) {
+      items = new List<Items>();
+      json['items'].forEach((v) {
+        items.add(new Items.fromJson(v));
+      });
+    }
+    returned = json['returned'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['available'] = this.available;
+    data['collectionURI'] = this.collectionURI;
+    if (this.items != null) {
+      data['items'] = this.items.map((v) => v.toJson()).toList();
+    }
+    data['returned'] = this.returned;
+    return data;
+  }
 }
