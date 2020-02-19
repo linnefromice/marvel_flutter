a7ef7c641e7e04289b0f87792806cdceeb410683
linnefromice <linnefromice@gmail.com>
2020-02-19 09:42:44 +0900

    add back button of TodayMarvelCharacterScreen

--- lib/screens/TodayMarvelCharacterScreen.dart
+++ lib/screens/TodayMarvelCharacterScreen.dart
@@ -4,6 +4,14 @@ class TodayMarvelCharacterScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+      ),
       body: Center(
         child: Text('Today\'s Marvel Character'),
       ),
