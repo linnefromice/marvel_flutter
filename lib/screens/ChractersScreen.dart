4e9f4f5054719e665f6d09da9fab5064c8662a50
linnefromice <linnefromice@gmail.com>
2020-02-19 09:18:41 +0900

    add MainScreen (add Drawer, movie list)

---
+++ lib/screens/ChractersScreen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:marvel_flutter/models/CharactersResult.dart';
+import 'package:marvel_flutter/models/domain/Character.dart';
+import 'package:marvel_flutter/services/MarvelApiService.dart';
+
+class CharactersScreen extends StatelessWidget {
+
+  Widget _buildCharacterList(final CharactersResult result) {
+    final List<Character> list = result.data.results;
+
+    return ListView.builder(
+      itemCount: list.length,
+      itemBuilder: (context, index) {
+        final Character item = list[index];
+        return ListTile(
+          title: Text(item.name),
+          subtitle: Text(item.description),
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Marvel Application"),
+      ),
+      body: Center(
+        child: FutureBuilder<CharactersResult>(
+          future: MarvelApiService.fetchCharacters(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return _buildCharacterList(snapshot.data);
+            } else if (snapshot.hasError) {
+              return Text("${snapshot.error}");
+            }
+            return CircularProgressIndicator();
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        tooltip: 'refresh',
+        child: Icon(Icons.refresh),
+        onPressed: () {
+          MarvelApiService.fetchCharacters();
+        },
+      ),
+    );
+  }
+}
--- lib/screens/MainScreen.dart
+++ lib/screens/MainScreen.dart
@@ -1,51 +1,79 @@
 import 'package:flutter/material.dart';
-import 'package:marvel_flutter/models/CharactersResult.dart';
-import 'package:marvel_flutter/models/domain/Character.dart';
-import 'package:marvel_flutter/services/MarvelApiService.dart';
+import 'package:marvel_flutter/screens/ChractersScreen.dart';
 
 class MainScreen extends StatelessWidget {
-
-  Widget _buildCharacterList(final CharactersResult result) {
-    final List<Character> list = result.data.results;
-
-    return ListView.builder(
-      itemCount: list.length,
-      itemBuilder: (context, index) {
-        final Character item = list[index];
-        return ListTile(
-          title: Text(item.name),
-          subtitle: Text(item.description),
-        );
-      },
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
+
+    Widget _buildDrawer() {
+      return(
+        Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              DrawerHeader(
+                child: Text('Menu'),
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                ),
+              ),
+              ListTile(
+                title: Text('Characters'),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => CharactersScreen()),
+                  );
+                },
+              ),
+            ],
+          ),
+        )
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Marvel Application"),
       ),
+      drawer: _buildDrawer(),
       body: Center(
-        child: FutureBuilder<CharactersResult>(
-          future: MarvelApiService.fetchCharacters(),
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return _buildCharacterList(snapshot.data);
-            } else if (snapshot.hasError) {
-              return Text("${snapshot.error}");
-            }
-            return CircularProgressIndicator();
-          },
+        child: ListView(
+          children: <Widget>[
+            ListTile(
+              leading: Text("1"),
+              title: Text("Iron Man"),
+              subtitle: Text("May 2, 2008"),
+            ),
+            ListTile(
+              leading: Text("2"),
+              title: Text("The Incredible Hulk"),
+              subtitle: Text("June 13, 2008"),
+            ),
+            ListTile(
+              leading: Text("3"),
+              title: Text("Iron Man 2"),
+              subtitle: Text("May 7, 2010"),
+            ),
+            ListTile(
+              leading: Text("4"),
+              title: Text("Thor"),
+              subtitle: Text("May 6, 2011"),
+            ),
+            ListTile(
+              leading: Text("5"),
+              title: Text("Captain America: The First Avenger"),
+              subtitle: Text("July 22, 2011"),
+            ),
+            ListTile(
+              leading: Text("6"),
+              title: Text("Marvel's The Avengers"),
+              subtitle: Text("May 4, 2012"),
+            ),
+          ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        tooltip: 'refresh',
-        child: Icon(Icons.refresh),
-        onPressed: () {
-          MarvelApiService.fetchCharacters();
-        },
-      ),
+      )
     );
   }
+
 }
