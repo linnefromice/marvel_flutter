ad0bf331b17ed67815cfc0fab3488529c0583016
linnefromice <linnefromice@gmail.com>
2020-03-10 18:14:20 +0900

    sepalate original model from MainScreen (named Movie)

---
+++ lib/models/original/Movie.dart
@@ -0,0 +1,9 @@
+class Movie {
+  final int id;
+  final int phase;
+  final String title;
+  final String date;
+  final String url;
+
+  Movie(this.id, this.phase, this.title, this.date, this.url);
+}
